6b2443933996a5af67e1bdc990db35fcf7fa33c1
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-12-21 17:09:22 -0600

    new test practice 1 addremove

---
+++ addandremove.test.js
@@ -0,0 +1,14 @@
+/*
+  * @jest-environment jsdom
+ */
+
+import ToDoList from "./src/modules/ToDoList.js";
+import Task from "./src/modules/Task.js";
+
+test('testing adding task', () => {
+  const toDoList = new ToDoList();
+  const task = new Task('task 1', 0);
+  toDoList.addTask(task);
+  expect(toDoList.taskList.length).toBe(1);
+});
+
