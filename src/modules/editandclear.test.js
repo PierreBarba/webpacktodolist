64ee78634cd78d7a7d408feba8ed71b688cb5fa7
HunterX-7 <fraquin7@gmail.com>
2022-12-22 11:48:01 -0500

    Add edit and clear test file

---
+++ src/modules/editandclear.test.js
@@ -0,0 +1,16 @@
+/*
+  * @jest-environment jsdom
+ */
+
+import ToDoList from './ToDoList.js';
+import Task from './Task.js';
+
+describe('Testing edit and clear function', () => {
+    test('Testing edit task description', ()=> {
+        const toDoList = new ToDoList();
+        const task = new Task('todo 1', 0);
+        toDoList.addTask(task);
+        task.description.getDescription('tarea 1');
+        expect(task.description).toBe('tarea 1');
+    })
+});
