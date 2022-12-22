78acfb95c2190441da3b169fb2863c72101498b1
HunterX-7 <fraquin7@gmail.com>
2022-12-21 23:07:45 -0500

    Add test for the remove function

--- addandremove.test.js
+++ addandremove.test.js
@@ -5,9 +5,23 @@
 import ToDoList from './src/modules/ToDoList.js';
 import Task from './src/modules/Task.js';
 
-test('testing adding task', () => {
-  const toDoList = new ToDoList();
-  const task = new Task('task 1', 0);
-  toDoList.addTask(task);
-  expect(toDoList.taskList.length).toBe(1);
+describe('Test add and remove', () => {
+  test('testing adding task', () => {
+    const toDoList = new ToDoList();
+    const task = new Task('task 1', 0);
+    toDoList.addTask(task);
+    expect(toDoList.taskList.length).toBe(1);
+  });
+  
+  test('testing remove task', () => {
+    let toDoList = new ToDoList();
+    let task = new Task('task 1', 0);
+    toDoList.addTask(task);
+    task = new Task('task 2', 1);
+    toDoList.addTask(task); 
+    task = new Task('task 3', 2);    
+    toDoList.addTask(task);
+    toDoList.removeTask(1);
+    expect(toDoList.taskList.length).toBe(2);
+  });
 });
